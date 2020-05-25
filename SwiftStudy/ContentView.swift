ae101e44dd6e50ca4d57c6bd13ec604263975dc8
boksil2 <hwoo3303@naver.com>
2020-05-25 23:37:30 +0900

    test commit

---
+++ SwiftStudy/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftStudy
+//
+//  Created by boksil2 on 2020/05/25.
+//  Copyright © 2020 boksil2. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
